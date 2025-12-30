4dde8f2320be74ea4e91b16307b85a3eb9635677
Leap-Chanvuthy <chanvuthy.leap@student.cadt.edu.kh>
2025-12-30 16:34:20 +0700

    .

--- config/app.php
+++ config/app.php
@@ -17,6 +17,8 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'frontend_url' => env('APP_FRONTEND_URL', 'http://localhost:5173'),
+
 
     /*
     |--------------------------------------------------------------------------
