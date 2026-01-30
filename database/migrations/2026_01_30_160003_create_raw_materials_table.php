3851719a80838f88c2ff5f3e60cbe9c532ec7a3a
Leap-Chanvuthy <chanvuthy.leap@student.cadt.edu.kh>
2026-01-30 23:50:11 +0700

    implement raw material

---
+++ database/migrations/2026_01_30_160003_create_raw_materials_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('raw_materials', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('material_name' , 50);
+            $table -> string('material_sku_code' , 255) -> unique();
+            $table->  string('barcode')->nullable();
+            $table -> double('quantity');
+            $table -> double('remaining_quantity');
+            $table -> double('minimum_quantity_stock_level');
+            $table -> double('unit_price_in_usd');
+            $table -> double('total_value_in_usd'); 
+            $table -> double('exchange_rate_from_usd_to_riel');
+            $table -> double('unit_price_in_riel');
+            $table -> double('total_value_in_riel');
+            $table -> double('exchange_rate_from_riel_to_usd');
+            $table -> date('expiry_date');
+            $table -> string('status' , 100);
+            $table -> text('description') -> nullable();
+
+            $table -> foreignId('raw_material_category_id') ->constrained('raw_material_categories')  -> restrictOnDelete() -> cascadeOnUpdate();
+            $table -> foreignId('uom_id') -> constrained('unit_of_measurements') -> restrictOnDelete() -> cascadeOnUpdate();
+            $table -> foreignId('supplier_id') -> constrained('suppliers') -> restrictOnDelete() -> cascadeOnUpdate();
+            $table -> foreignId('warehouse_id') -> constrained('warehouses') -> restrictOnDelete() -> cascadeOnUpdate();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('raw_materials');
+    }
+};
